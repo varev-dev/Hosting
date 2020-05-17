fbc216e39d2943f8326def7238f616fe20404efe
varev-dev <KACDOGA4@edu.gdansk.pl>
2020-05-17 21:09:11 +0200

    working store count & cookie
    oj byczku rozjebałem kompletnie

--- js/storeSys.js
+++ js/storeSys.js
@@ -1,41 +1,57 @@
-var vrvCountVPS = 0;
+var vrvCountVPS = 0; 
+vrvCountVPS = parseInt(localStorage.getItem('VPS'), 10);
+if(localStorage.getItem('VPS') == null){
+    vrvCountVPS = 0; 
+}
+
 var vrvCountCloud = 0;
+vrvCountCloud = parseInt(localStorage.getItem('Cloud'), 10);
+if(localStorage.getItem('Cloud') == null){
+    vrvCountCloud = 0; 
+}
+
 var vrvCountDB = 0;
+vrvCountDB = parseInt(localStorage.getItem('DB'), 10);
+if(localStorage.getItem('Cloud') == null){
+    vrvCountDB = 0;
+}
+
 var vrvItems = 0;
+vrvItems = parseInt(localStorage.getItem('Total'), 10);
+if(localStorage.getItem('Total') == null){
+    vrvItems = 0;
+}
+document.getElementById('vrvCountItems').innerHTML = vrvItems;
 
 function vrvVPSAddIntoBasket() {
     vrvItems += 1;
     vrvCountVPS += 1;
-    console.log(vrvCountVPS);
-
     localStorage.setItem('VPS', vrvCountVPS);
+    vrvCountVPS = parseInt(localStorage.getItem('VPS'), 10);
+    vrvTotalItems();
     console.log(localStorage);
-
-    vrvCountItems();
 }
 
 function vrvCloudAddIntoBasket() {
     vrvItems += 1;
     vrvCountCloud += 1;
-    console.log(vrvCountCloud);
-
     localStorage.setItem('Cloud', vrvCountCloud);
+    vrvCountCloud = parseInt(localStorage.getItem('Cloud'), 10);
+    vrvTotalItems();
     console.log(localStorage);
-
-    vrvCountItems();
 }
 
 function vrvDBAddIntoBasket() {
     vrvItems += 1;
     vrvCountDB += 1;
-    console.log(vrvCountDB);
-
     localStorage.setItem('DB', vrvCountDB);
+    vrvCountDB = parseInt(localStorage.getItem('DB'), 10);
+    vrvTotalItems();
     console.log(localStorage);
-
-    vrvCountItems();
 }
 
-function vrvCountItems() {
-    console.log(vrvItems);
+function vrvTotalItems() {
+    localStorage.setItem('Total', vrvItems);
+    vrvItems = parseInt(localStorage.getItem('Total'), 10);
+    document.getElementById('vrvCountItems').innerHTML = vrvItems;
 }
